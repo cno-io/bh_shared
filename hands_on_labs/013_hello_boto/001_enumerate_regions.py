c8198d9b86d485e4613a7cbf15d826adfe37bae4
cno-io <40723360+cno-io@users.noreply.github.com>
2018-10-14 22:51:59 -0600

    Update 001_enumerate_regions.py

--- hands_on_labs/013_hello_boto/001_enumerate_regions.py
+++ hands_on_labs/013_hello_boto/001_enumerate_regions.py
@@ -1,5 +1,5 @@
 import boto3
-session = boto3.Session(profile_name='lambdavuln')
+session = boto3.Session(profile_name='vulnlambda')
 ec2 = session.client("ec2",)
 regions = ec2.describe_regions()
 regions = [r['RegionName'] for r in regions['Regions']]
