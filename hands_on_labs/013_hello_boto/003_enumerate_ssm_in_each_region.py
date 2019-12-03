0c9ed3bf806f8fc18b20eb9c982a38088ed98b82
cno-io <40723360+cno-io@users.noreply.github.com>
2019-12-03 09:36:04 +0000

    Update 003_enumerate_ssm_in_each_region.py

--- hands_on_labs/013_hello_boto/003_enumerate_ssm_in_each_region.py
+++ hands_on_labs/013_hello_boto/003_enumerate_ssm_in_each_region.py
@@ -1,6 +1,6 @@
 import boto3
 session = boto3.Session(profile_name='vulnlambda')
-ec2 = session.client("ec2",)
+ec2 = session.client("ec2", "us-east-1")
 regions = ec2.describe_regions()
 regions = [r['RegionName'] for r in regions['Regions']]
 for r in regions:
