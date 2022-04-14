9ebee7ccd7dd77e2e4a0f39a8e70b38105329bb9
cno-io <40723360+cno-io@users.noreply.github.com>
2022-04-13 18:11:47 -0600

    Update 003_enumerate_ssm_in_each_region.py

--- hands_on_labs/013_hello_boto/003_enumerate_ssm_in_each_region.py
+++ hands_on_labs/013_hello_boto/003_enumerate_ssm_in_each_region.py
@@ -4,6 +4,7 @@ ec2 = session.client("ec2", "us-east-1")
 regions = ec2.describe_regions()
 regions = [r['RegionName'] for r in regions['Regions']]
 for r in regions:
+    print("Region: " + r)
     ssmClient = session.client("ssm", r)
     params = ssmClient.describe_parameters()
     for p in params['Parameters']:
@@ -11,4 +12,4 @@ for r in regions:
         response = ssmClient.get_parameter(
             Name=p_name)
         val = response['Parameter']['Value']
-        print "%s - %s" % (p_name, val)
+        print(str(p_name) + " - " + str(val))
